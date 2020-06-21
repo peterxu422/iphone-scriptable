e89e2e83fecd6afaedab4979168d2916b6e971b7
Peter Xu <xupeter@amazon.com>
2020-06-21 15:49:46 -0400

    Add scripts directory. Create Deepak script

---
+++ scripts/deepak.js
@@ -0,0 +1,5 @@
+
+let html = `
+<iframe src="http://wisdomofchopra.com/" frameborder=no style="-webkit-transform: scale(1.75);  -webkit-transform-origin: 0 0;"></iframe>
+`
+WebView.loadHTML(html, null, new Size(0, 400))
