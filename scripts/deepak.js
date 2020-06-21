c175c9945f55170309d0d470e72dac736bf9ed24
Peter Xu <xupeter@amazon.com>
2020-06-21 16:06:19 -0400

    Update Deepak using Safari to load page

--- scripts/deepak.js
+++ scripts/deepak.js
@@ -1,5 +1,3 @@
-
-let html = `
-<iframe src="http://wisdomofchopra.com/" frameborder=no style="-webkit-transform: scale(1.75);  -webkit-transform-origin: 0 0;"></iframe>
-`
-WebView.loadHTML(html, null, new Size(0, 400))
+// Hit me with that Deepak wisdom.
+let url='http://wisdomofchopra.com/';
+Safari.openInApp(url);
